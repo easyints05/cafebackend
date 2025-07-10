035d2f0376e3afe0f442b21ec117fa08848165bc
easyint05 <akshayasakhi@gmail.com>
2025-07-10 16:40:56 +0530

    done

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRoute.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpassword = encodeURIComponent(process.env.DBPASSWORD);
 
 mongoose.connect(`mongodb+srv://${dbuser}:${dbpassword}@cluster0.llljuvp.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
   app.listen(8081, () => {
-    console.log("Server started");
+    console.log("Server started at port 8081");
   });
 });
 app.use("/api/users", userRouter);
